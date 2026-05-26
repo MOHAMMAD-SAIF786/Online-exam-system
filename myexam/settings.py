3c165719475903e5e5e3a91a9f2e93b9c201f3a1
MOHAMMAD SAIF <mdsaif123654@gmail.com>
2026-05-27 00:47:35 +0530

    static fix

--- myexam/settings.py
+++ myexam/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,5 +131,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"   
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
